a218076904fdd8f53379feb4191c4d698c1ff9bd
Saullo <saullorhamon@gmail.com>
2023-03-16 23:50:12 -0300

    alteracao

---
+++ services/projects.ts
@@ -0,0 +1,35 @@
+export const projects = [
+  {
+    title: "Order summary component challenge frontend mentor",
+    href: "#",
+    category: { name: "Projeto", href: "#" },
+    description:
+      "Projeto de desafio do site frontend mentor, onde era proposto criar um layout de uma ordem de pagamento para determinado produto.",
+    imageUrl:
+      "https://github.com/saulloalmeida/order-summary-component/raw/master/screen.png?raw=true",
+    demo:"https://order-summary-component-umber-omega.vercel.app/",
+    github:"https://github.com/saulloalmeida/order-summary-component"
+  },
+  {
+    title: "Simple BMI / IMC",
+    href: "#",
+    category: { name: "Projeto", href: "#" },
+    description:
+      "Projeto de uma Calculadora de IMC - Simple Calculator BMI",
+    imageUrl:
+      "https://raw.githubusercontent.com/saulloalmeida/simple-imc/main/project-screen.png?raw=true",
+    demo:"https://simple-imc.vercel.app/",
+    github:"https://github.com/saulloalmeida/simple-imc"
+  },
+  {
+    title: "BuscaCep 👋",
+    href: "#",
+    category: { name: "Projeto", href: "#" },
+    description:
+      "Aplicação simples para buscar cep consumindo API viaCEP",
+    imageUrl:
+      "https://github.com/saulloalmeida/busca-cep/raw/main/project-screen.png?raw=true",
+    demo:"https://busca-cep-lemon.vercel.app/",
+    github:"https://github.com/saulloalmeida/busca-cep"
+  },
+];
---
+++ services/skills.ts
@@ -0,0 +1,68 @@
+import { IconType } from "react-icons";
+import { AiFillHtml5 } from "react-icons/ai";
+import { DiCss3, DiReact } from "react-icons/di";
+import { FaNodeJs } from "react-icons/fa";
+import {
+  SiExpress,
+  SiJavascript,
+  SiPrisma,
+  SiTypescript,
+  SiNextdotjs
+} from "react-icons/si";
+
+export const freatures = [
+  {
+    name: "HTML",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+    icon: AiFillHtml5,
+  },
+  {
+    name: "CSS",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+    icon: DiCss3,
+  },
+  {
+    name: "Javascript",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+    icon: SiJavascript,
+  },
+  {
+    name: "Typescript",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+    icon: SiTypescript,
+  },
+  {
+    name: "React",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+    icon: DiReact,
+  },
+  {
+    name: "Prisma",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+    icon: SiPrisma,
+  },
+  {
+    name: "Express",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+    icon: SiExpress,
+  },
+  {
+    name: "Node",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+    icon: FaNodeJs,
+  },
+  {
+    name: "Next",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+    icon: SiNextdotjs,
+  },
+];
